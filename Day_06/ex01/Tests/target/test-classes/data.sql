2417818936106466a8e52ebee948697ec0c251db
ayglazkov <gtaggana@student.21-school.ru>
2022-07-30 22:24:45 +0300

    add Day09

---
+++ Day_06/ex01/Tests/target/test-classes/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO product(id, name, price) VALUES
+(1, 'Tea', 116.99),
+(2, 'Coffee', 634.99),
+(3, 'Chocolate', 80.50),
+(4, 'Whiskey', 899.99),
+(5, 'Crisps', 139.99),
+(6, 'Milk', 72.50);
